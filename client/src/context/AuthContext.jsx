959f211744f37282031cf442e9501c4239e12786
and1kmpo <acampolpez@gmail.com>
2023-10-25 13:52:14 -0500

    Describe your changes here

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useState, useContext, useEffect } from "react";
-import { registerRequest, loginRequest } from '../api/auth';
+import { registerRequest, loginRequest, verifyTokenRequest } from '../api/auth';
+import Cookies from 'js-cookie'
 
 export const AuthContext = createContext();
 
@@ -15,6 +16,7 @@ export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [errors, setErrors] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     const signup = async (user) => {
         try {
@@ -31,14 +33,13 @@ export const AuthProvider = ({ children }) => {
     const signin = async (user) => {
         try {
             const res = await loginRequest(user);
-            console.log(res);
+            setUser(res.data);
+            setIsAuthenticated(true);
         } catch (error) {
-            if (Array.isArray(error.response.data)) {
-                return setErrors(error.response.data);
-            }
-            setErrors([error.response.data.message]);
+            console.log(error);
+            // setErrors(error.response.data.message);
         }
-    }
+    };
 
     useEffect(() => {
         if (errors.length > 0) {
@@ -49,8 +50,32 @@ export const AuthProvider = ({ children }) => {
         }
     }, [errors]);
 
+    useEffect(() => {
+        const checkLogin = async () => {
+            const cookies = Cookies.get();
+            if (!cookies.token) {
+                setIsAuthenticated(false);
+                setLoading(false);
+                return;
+            }
+
+            try {
+                const res = await verifyTokenRequest(cookies.token);
+                console.log(res);
+                if (!res.data) return setIsAuthenticated(false);
+                setIsAuthenticated(true);
+                setUser(res.data);
+                setLoading(false);
+            } catch (error) {
+                setIsAuthenticated(false);
+                setLoading(false);
+            }
+        };
+        checkLogin();
+    }, []);
+
     return (
-        <AuthContext.Provider value={{ signup, signin, user, isAuthenticated, errors }}>
+        <AuthContext.Provider value={{ signup, signin, user, isAuthenticated, errors, loading }}>
             {children}
         </AuthContext.Provider>
     )
---
+++ client/src/pages/TaskFormPage.jsx
@@ -0,0 +1,26 @@
+import { useForm } from 'react-hook-form'
+import { useTasks } from '../context/TaskContext';
+
+function TaskFormPage() {
+    const { register, handleSubmit } = useForm();
+    const { createTask } = useTasks();
+
+
+    const onSubmit = handleSubmit((data) => {
+        createTask(data);
+    })
+
+    return (
+        <div className='bg-zinc-800 max-w-md w-full p-10 rounded-md'>
+            <form onSubmit={onSubmit}>
+                <input type="text" placeholder='Title' {...register('title')} autoFocus className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2' />
+                <textarea rows="3" placeholder='Description' {...register('description')} className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2' ></textarea>
+                <button type="submit">
+                    Save
+                </button>
+            </form>
+        </div>
+    )
+}
+
+export default TaskFormPage
